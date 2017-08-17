8e2265559321b96547a6121acbe2e985066ccf7c
stephanielamm <slamm5@live.unc.edu>
2017-08-17 16:26:00 -0400

    Button functionality.

--- bubbles.js
+++ bubbles.js
@@ -1,6 +1,6 @@
 (function() {
-var width = 700,
-  height = 700;
+var width = 900,
+  height = 900;
 
 var svg = d3.select("#chart")
   .append("svg")
@@ -11,12 +11,24 @@ var svg = d3.select("#chart")
 
 var radiusScale = d3.scaleSqrt().domain([900, 3000000]).range([10,50])
 
+var forceXSeparate = d3.forceX(function(d){
+  if(d.jurisdiction === 'Atlanta') {
+    return 250
+  } else {
+    return 700
+  }
+}).strength(0.05)
+
+var forceXCombine = d3.forceX(width / 2).strength(0.05)
+
+var forceCollide = d3.forceCollide(function(d) {
+  return radiusScale(d.abated) + 5;
+})
+
 var simulation = d3.forceSimulation()
-  .force("x", d3.forceX(width / 2).strength(0.05))
+  .force("x", forceXCombine)
   .force("y", d3.forceY(height /2).strength(0.05))
-  .force("collide", d3.forceCollide(function(d) {
-    return radiusScale(d.abated) + 3;
-  }))
+  .force("collide", forceCollide)
 
 d3.queue()
   .defer(d3.csv, "taxes.csv")
@@ -32,6 +44,20 @@ function ready (error, datapoints) {
       return radiusScale(d.abated);
     })
     .attr("fill", "lightblue")
+
+  d3.select("#jurisdiction").on('click', function(){
+    simulation
+    .force("x", forceXSeparate)
+    .alphaTarget(0.29)
+    .restart()
+  })
+
+  d3.select("#combine").on('click', function(){
+    simulation
+    .force("x", forceXCombine)
+    .alphaTarget(0.2)
+    .restart()
+  })
 
   simulation.nodes(datapoints)
     .on('tick', ticked)
