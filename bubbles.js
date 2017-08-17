c420abd73d964e585a36098f350e8319337a6c7b
stephanielamm <slamm5@live.unc.edu>
2017-08-17 15:34:05 -0400

    Basic bubble chart created
    No labels or tooltip.

--- bubbles.js
+++ bubbles.js
@@ -9,10 +9,14 @@ var svg = d3.select("#chart")
   .append("g")
   .attr("transform", "translate(0,0)")
 
+var radiusScale = d3.scaleSqrt().domain([1, 40000]).range([10,80])
+
 var simulation = d3.forceSimulation()
   .force("x", d3.forceX(width / 2).strength(0.05))
   .force("y", d3.forceY(height /2).strength(0.05))
-  .force("collide", d3.forceCollide(15))
+  .force("collide", d3.forceCollide(function(d) {
+    return radiusScale(d.abated) + 3;
+  }))
 
 d3.queue()
   .defer(d3.csv, "taxes.csv")
@@ -24,7 +28,9 @@ function ready (error, datapoints) {
     .data(datapoints)
     .enter().append("circle")
     .attr("class", "company")
-    .attr("r", 10)
+    .attr("r", function(d){
+      return radiusScale(d.abated);
+    })
     .attr("fill", "lightblue")
 
   simulation.nodes(datapoints)
